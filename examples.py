eb16d0d6b01899acc9e8500f16adb4ac44e5b399
Pawel Paradysz <pa.paradysz@gmail.com>
2019-10-27 10:35:37 +0000

    examples

---
+++ examples.py
@@ -0,0 +1,6 @@
+one_day = {'weekDay':'Monday', 'date':'01/11/2019', 'slots' : [[] for i in range(48)]]}  # empty list for ids of events
+
+event = {'socId':'0', 'date': '01/11/2019', 'start': 30, 'end': 35}
+
+society = {'mailingList':[]}
+
