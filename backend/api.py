9b3d2e7b8e5400d0c284bbeae68276007dc1a0a5
fryderyk <frydaw09@gmail.com>
2019-10-27 13:39:26 +0000

    back <-> front end communication

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,49 @@
-import flask
+import flask, model
 from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+model = Model()
+communities = list(model.communities.find())
+days = list(model.days.find())
+events = list(model.events.find())
 
+
+#front -> back
+
+@app.route('/', methods = ['GET'])
+def add_event(community_name, date, start_slot, end_slot):
+  model.assert_days_present(date, date)
+  community = model.communities.find_one({"name" : community_name})
+  model.add_event(community["_id"], date, start_slot, end_slot)  
+
+#back -> front
+
+@app.route('/', methods = ['GET'])
+def get_calendar(community, start_date):
+  our_mails = community["mailingList"]
+
+  calendar = [[[] for i in range(48)] for j in range(7)]
+  for i in range(7):
+    date = start_date + datetime.timedelta(days=i)
+    db_day = model.days.find_one({"date" : str(date)})
+
+    for db_slot, cal_slot in zip(db_day["slots"], calendar[i]):
+      for event_id in db_slot:
+        event = model.events.find_one({"_id":ObjectId(event_id)})
+        comm_id = event["comm_id"]
+        other_community = model.events.find_one({"_id":ObjectId(comm_id)})
+
+        for mail in other_community["mailingList"]:
+          if our_mails.contains(mail):
+            cal_slot += mail
+
+  result = [[0 for i in range(48)] for j in range(7)]
+
+  for i in range(7):
+    for j in range(48):
+      result[i][j] = double(set(calendar[i][j]).size) / double(our_mails.size)
+
+  return jsonify(result)
+  
